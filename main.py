cb687cd0c6193612d43bc44f1fd5fea2564486d4
cbr <cbr@ths.school.nz>
2024-11-10 20:12:35 +1300

    Most of kahoot completed

--- main.py
+++ main.py
@@ -129,19 +129,22 @@ import time
 #
 # print(quiz_list)
 
-from QuizBot import QuizBot
-
-bot = QuizBot(
-    url="https://www.stuff.co.nz/quizzes/350407318/stuff-quiz-morning-trivia-challenge-october-29-2024",
-    username="cbriggsnz1977@gmail.com",
-    password="?NtLdRR8NQQff$3g",
-    debug=False
-)
+# from QuizBot import QuizBot
+#
+# bot = QuizBot(
+#     url="https://www.stuff.co.nz/quizzes/350407318/stuff-quiz-morning-trivia-challenge-october-29-2024",
+#     username="cbriggsnz1977@gmail.com",
+#     password="?NtLdRR8NQQff$3g",
+#     debug=False
+# )
 # bot.mainLoop()
 
-quiz = bot.run_quiz()
+# quiz = bot.run_quiz()
+quiz = {'Title': 'Stuff quiz: Morning trivia challenge: October 29, 2024', 'Quiz_Data': [{'Correct': 1, 'Answers': ['Hack shack', 'Chop shop', 'Whip strip', 'Wreck deck'], 'Question': 'A place where stolen vehicles are dismantled so that the parts can be sold or used to repair other stolen vehicles is called a ...'}, {'Correct': 1, 'Answers': ['Gerald Ford', 'Richard Nixon', 'Lyndon B Johnson', 'Dwight D Eisenhower'], 'Question': 'Which US president was at the centre of the Watergate scandal?'}, {'Correct': 2, 'Answers': ['Rampart', 'Keep', 'Embrasure', 'Portcullis'], 'Question': 'In a medieval castle, what name was given to an opening in the wall from which cannons were fired?'}, {'Correct': 0, 'Answers': ['Eroica', 'Pastorale'], 'Question': "What is the alternate name for Beethoven's Third Symphony?"}, {'Correct': 1, 'Answers': ['Tears', 'Ear wax', 'Urine', 'Stomach acid'], 'Question': 'Cerumen is more commonly known as ...'}, {'Correct': 1, 'Answers': ['Djibouti', 'Eritrea', 'Somalia', 'South Sudan'], 'Question': 'Which country broke away from Ethiopia in 1991?'}, {'Correct': 0, 'Answers': ['Echinoderms', 'Pinnipeds', 'Fissipeds', 'Crustaceans'], 'Question': 'Sea urchins belong to which marine animal group?'}, {'Correct': 2, 'Answers': ['A piece of playground equipment', 'A police informant', 'A bar with an honour system', 'A small stove'], 'Question': 'What is an "Honest John"?'}, {'Correct': 2, 'Answers': ['Paris', 'Tokyo', 'Rome', 'London'], 'Question': 'According to the saying, which city wasn\'t "built in a day"?'}, {'Correct': 0, 'Answers': ['Lettuce', 'Tomatoes', 'Potatoes', 'Cucumber'], 'Question': 'What produce might be butterhead, iceberg, or romaine?'}, {'Correct': 0, 'Answers': ['Uptown Girl', 'Always a Woman', "We Didn't Start the Fire", 'Piano Man'], 'Question': 'What Billy Joel song was inspired by a night out with Christie Brinkley, Whitney Houston and Elle Macpherson?'}, {'Correct': 1, 'Answers': ['A cat', 'A cow', 'A duck', 'A goat'], 'Question': 'According to legend, what kind of animal caused the great Chicago fire of 1871?'}, {'Correct': 0, 'Answers': ['Five', 'Six'], 'Question': 'How many points are on a single star on the American flag?'}, {'Correct': 2, 'Answers': ['Tongue', 'Throat', 'Toe', 'Tonsils'], 'Question': 'Bunions affect which body part?'}, {'Correct': 0, 'Answers': ['Married', 'Brother and sister-in-law', 'Cousins', 'Twins'], 'Question': 'Actors Nick Offerman and Megan Mullally are ...'}]}
+
 print(quiz)
 # print(len(quiz_data))
+
 quiz_data = quiz["Quiz_Data"]
 # quiz_data = [{'Correct': 2, 'Answers': ['Hack shack', 'Wreck deck', 'Chop shop', 'Whip strip'], 'Question': 'A place where stolen vehicles are dismantled so that the parts can be sold or used to repair other stolen vehicles is called a ...'}, {'Correct': 2, 'Answers': ['Dwight D Eisenhower', 'Lyndon B Johnson', 'Richard Nixon', 'Gerald Ford'], 'Question': 'Which US president was at the centre of the Watergate scandal?'}, {'Correct': 2, 'Answers': ['Keep', 'Portcullis', 'Embrasure', 'Rampart'], 'Question': 'In a medieval castle, what name was given to an opening in the wall from which cannons were fired?'}, {'Correct': 0, 'Answers': ['Eroica', 'Pastorale'], 'Question': "What is the alternate name for Beethoven's Third Symphony?"}, {'Correct': 0, 'Answers': ['Ear wax', 'Stomach acid', 'Tears', 'Urine'], 'Question': 'Cerumen is more commonly known as ...'}, {'Correct': 0, 'Answers': ['Eritrea', 'Somalia', 'Djibouti', 'South Sudan'], 'Question': 'Which country broke away from Ethiopia in 1991?'}, {'Correct': 1, 'Answers': ['Crustaceans', 'Echinoderms', 'Fissipeds', 'Pinnipeds'], 'Question': 'Sea urchins belong to which marine animal group?'}, {'Correct': 2, 'Answers': ['A police informant', 'A small stove', 'A bar with an honour system', 'A piece of playground equipment'], 'Question': 'What is an "Honest John"?'}, {'Correct': 1, 'Answers': ['Tokyo', 'Rome', 'Paris', 'London'], 'Question': 'According to the saying, which city wasn\'t "built in a day"?'}, {'Correct': 2, 'Answers': ['Potatoes', 'Cucumber', 'Lettuce', 'Tomatoes'], 'Question': 'What produce might be butterhead, iceberg, or romaine?'}, {'Correct': 1, 'Answers': ['Piano Man', 'Uptown Girl', "We Didn't Start the Fire", 'Always a Woman'], 'Question': 'What Billy Joel song was inspired by a night out with Christie Brinkley, Whitney Houston and Elle Macpherson?'}, {'Correct': 2, 'Answers': ['A duck', 'A goat', 'A cow', 'A cat'], 'Question': 'According to legend, what kind of animal caused the great Chicago fire of 1871?'}, {'Correct': 1, 'Answers': ['Six', 'Five'], 'Question': 'How many points are on a single star on the American flag?'}, {'Correct': 0, 'Answers': ['Toe', 'Tongue', 'Throat', 'Tonsils'], 'Question': 'Bunions affect which body part?'}, {'Correct': 2, 'Answers': ['Cousins', 'Twins', 'Married', 'Brother and sister-in-law'], 'Question': 'Actors Nick Offerman and Megan Mullally are ...'}]
 
@@ -223,7 +226,7 @@ wait_and_click(By.XPATH, '//div[@data-cta="blank"]')
 # Send text to <p> element
 for idx, question in enumerate(quiz_data):
     print(question)
-    wait_and_send_keys(By.CSS_SELECTOR, "p[data-placeholder='Start typing your question']", question["Question"] + Keys.ENTER)
+    wait_and_send_keys(By.CSS_SELECTOR, "p[data-placeholder='Start typing your question']", question["Question"][:120] + Keys.ENTER)
 
     # Enter answers into multiple fields
     answers = question["Answers"]
@@ -239,3 +242,9 @@ for idx, question in enumerate(quiz_data):
 
         # Click "Add a new Quiz type question" button
         wait_and_click(By.CSS_SELECTOR, 'button[data-functional-selector="create-button__quiz"]')
+
+wait_and_click(By.CSS_SELECTOR, 'button[data-functional-selector="top-bar__save-button"]')
+
+wait_and_send_keys(By.ID, "kahoot-title", quiz["Title"] + Keys.ENTER)
+
+wait_and_click(By.CSS_SELECTOR, 'button[data-functional-selector="dialog-add-title__continue"]')
